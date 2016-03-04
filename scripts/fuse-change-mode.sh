27700e67b04251c265420dd06d0b96775a7dcc0a
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-03-04 17:31:16 +0300

    scripts: return 0 in case of FUSE_DIR_PATTERN is not set
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- scripts/fuse-change-mode.sh
+++ scripts/fuse-change-mode.sh
@@ -2,15 +2,15 @@
 
 [[ "post-restore" == "$CRTOOLS_SCRIPT_ACTION" ]] || exit 0
 
-[ -z "$FUSE_CLIENT" ] && exit 1
-[ -z "$FUSE_DIR_PATTERN" ] && exit 2
-[ -z "$FUSE_MODE" ] && exit 3
+[ -n "$FUSE_DIR_PATTERN" ] || exit 0
+[ -n "$FUSE_CLIENT" ] || exit 1
+[ -n "$FUSE_MODE" ] || exit 2
 
 set -o pipefail
 
 for sock in $FUSE_DIR_PATTERN*/*.sock; do
 	$FUSE_CLIENT mode --mode $FUSE_MODE --socket_path $sock
-	[ $? -eq 0 ] || exit 4
+	[ $? -eq 0 ] || exit 3
 done
 
 exit 0
