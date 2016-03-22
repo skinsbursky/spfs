e17ed068f2afa6093bced4222fcd306121f4bd88
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-03-22 20:23:17 +0300

    manager: interface introduced
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

---
+++ include/spfs_manager_interface.h
@@ -0,0 +1,16 @@
+#ifndef __SPFS_INTERFACE_H_
+#define __SPFS_INTERFACE_H_
+
+#include "include/spfs_interface.h"
+
+enum {
+	SPFS_MANAGER_CMD_MOUNT_FS = SPFS_CMD_MAX,
+	SPFS_MANAGER_CMD_MAX,
+};
+
+TODO:
+1) Bypass spfs commands to spfs.
+2) Add mount command handling to manager
+3) Add mount command construction to client
+
+#endif
