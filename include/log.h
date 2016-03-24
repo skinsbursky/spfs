44204e596d2316ab92ab1b2c02020952292f8f33
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-03-24 18:46:10 +0300

    src/log: improve log header
    Now messages will be printed like this:
    <binary_name>(<process_pid>): <error_type.7>: <message>
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- include/log.h
+++ include/log.h
@@ -7,56 +7,60 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <syscall.h>
+#include <string.h>
 
 static inline pid_t gettid(void)
 {
 	return syscall(SYS_gettid);
-
 }
 
-#define pr_emerg(fmt, ...)			\
-	print_on_level(LOG_EMERG,		\
-			"EMERG : %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_alert(fmt, ...)			\
-	print_on_level(LOG_ALERT,		\
-			"ALERT : %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_crit(fmt, ...)			\
-	print_on_level(LOG_CRIT,		\
-			"CRIT  : %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_err(fmt, ...)			\
-	print_on_level(LOG_ERR,			\
-			"ERROR : %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_warn(fmt, ...)			\
-	print_on_level(LOG_WARNING,		\
-			"WARN  : %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_notice(fmt, ...)			\
-	print_on_level(LOG_NOTICE,		\
-			"NOTICE: %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_info(fmt, ...)			\
-	print_on_level(LOG_INFO,		\
-			"INFO  : %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_debug(fmt, ...)			\
-	print_on_level(LOG_DEBUG,		\
-			"DEBUG : %d: "fmt, gettid(), ##__VA_ARGS__)
-
-#define pr_perror(fmt, ...)			\
-({						\
-	int _errno = errno;			\
-        print_on_level(LOG_ERR,			\
-                       "ERROR : %d: "fmt": %d\n", gettid(),	\
-		       ##__VA_ARGS__, errno);	\
-	errno = _errno;				\
-})
+extern const char *__progname;
 
 int print_on_level_va(unsigned int level, const char *format, va_list args);
 int print_on_level(unsigned int level, const char *format, ...);
+
+#define print_with_header(verbosity, fmt, ...)			\
+({								\
+	char *v = #verbosity;					\
+								\
+	print_on_level(LOG_ ## verbosity,			\
+			"%s(%d): %s%*s: "fmt,			\
+			__progname, gettid(), v,		\
+			7 - strlen(v), "",			\
+			##__VA_ARGS__);				\
+})
+
+#define pr_emerg(fmt, ...)					\
+	print_with_header(EMERG, fmt, ##__VA_ARGS__)
+
+#define pr_alert(fmt, ...)					\
+	print_with_header(ALERT, fmt, ##__VA_ARGS__)
+
+#define pr_crit(fmt, ...)					\
+	print_with_header(CRIT, fmt, ##__VA_ARGS__)
+
+#define pr_err(fmt, ...)					\
+	print_with_header(ERR, fmt, ##__VA_ARGS__)
+
+#define pr_warn(fmt, ...)					\
+	print_with_header(WARNING, fmt, ##__VA_ARGS__)
+
+#define pr_notice(fmt, ...)					\
+	print_with_header(NOTICE, fmt, ##__VA_ARGS__)
+
+#define pr_info(fmt, ...)					\
+	print_with_header(INFO, fmt, ##__VA_ARGS__)
+
+#define pr_debug(fmt, ...)					\
+	print_with_header(DEBUG, fmt, ##__VA_ARGS__)
+
+#define pr_perror(fmt, ...)					\
+({								\
+	int _errno = errno;					\
+	print_with_header(ERR, fmt": %s\n", ##__VA_ARGS__,	\
+					strerror(errno));	\
+	errno = _errno;						\
+})
 
 void set_log_level(FILE *log, int level);
 int setup_log(const char *log_file, int verbosity);
