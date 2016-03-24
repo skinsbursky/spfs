3ad430a6c051260479adfed5d97f3d943a70bc78
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-03-24 15:59:04 +0300

    client: add "mount" command
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- client/main.c
+++ client/main.c
@@ -3,8 +3,31 @@
 #include <stdio.h>
 
 #include "spfs/interface.h"
+#include "manager/interface.h"
+
 #include "include/util.h"
 #include "include/socket.h"
+
+static int send_mount(const char *socket_path, const char *source, const char *type,
+		      unsigned long mountflags, const char *options)
+{
+	size_t len;
+	struct external_cmd *package;
+
+	printf("mounting %s with flags %ld and options '%s'\n", type,
+			mountflags, options);
+
+	len = mount_packet_size(source, type, options);
+
+	package = malloc(len);
+	if (!package) {
+		printf("failed to allocate package\n");
+		return -ENOMEM;
+	}
+	fill_mount_packet(package, source, type, options, mountflags);
+
+	return send_packet(socket_path, package, len);
+}
 
 static int send_path(const char *socket_path, const char *path_to_send, const char *path_to_stat)
 {
@@ -56,6 +79,7 @@ static void help(char *program)
 	printf("commands:\n");
 	printf("\tmode            allows to change mode work mode.\n");
 	printf("\tpath            allows to send a path to be used in Golem mode.\n");
+	printf("\tmount           allows to request for mount a filesystem.\n");
 	printf("\n");
 	printf("general options:\n");
 	printf("\t-s   --socket_path     control socket bind path\n");
@@ -68,6 +92,77 @@ static void help(char *program)
 	printf("Path options:\n");
 	printf("\t--path_to_send    file path to send to spfs\n");
 	printf("\t--path_to_stat    file path to stat\n");
+	printf("\n");
+	printf("Mount options:\n");
+	printf("\t--source          fileystem fype source (default: \"none\"\n");
+	printf("\t--fstype          fileystem fype (string)\n");
+	printf("\t--mountflags      file system mount flags (default: 0)\n");
+	printf("\t--options         file system mount options (default: empty)\n");
+}
+
+static int execude_mount_cmd(int argc, char **argv)
+{
+	long mountflags = 0;
+	char *source = "none";
+	char *type = NULL;
+	char *options = NULL;
+	char *socket_path = NULL;
+	static struct option opts[] = {
+		{"source",		required_argument,	0,	's' },
+		{"mountflags",		required_argument,	0,	'f' },
+		{"fstype",		required_argument,	0,	't' },
+		{"options",		required_argument,	0,	'o' },
+		{"socket_path",		required_argument,	0,	3 },
+		{"help",		no_argument,		0,	'h'},
+		{0,			0,			0,	0 }
+	};
+
+	while (1) {
+		char c;
+
+		c = getopt_long(argc, argv, "f:t:o:h", opts, NULL);
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case 'f':
+				if (xatol(optarg, &mountflags))
+					printf("mountflags is not a number: %s\n", optarg);
+				break;
+			case 't':
+				type = optarg;
+				break;
+			case 's':
+				source = optarg;
+				break;
+			case 'o':
+				options = optarg;
+				break;
+			case 3:
+				socket_path = optarg;
+				break;
+			case 'h':
+				help(argv[0]);
+				return 0;
+			case '?':
+				help(argv[0]);
+				return 1;
+		}
+	}
+
+	if (!type) {
+		printf("File system type wasn't provided\n");
+		help(argv[0]);
+		return 1;
+	}
+
+	if (!socket_path) {
+		printf("Socket path wasn't provided\n");
+		help(argv[0]);
+		return 1;
+	}
+
+	return send_mount(socket_path, source, type, mountflags, options);
 }
 
 static int execude_path_cmd(int argc, char **argv)
@@ -105,7 +200,7 @@ static int execude_path_cmd(int argc, char **argv)
 			case '?':
 				help(argv[0]);
 				return 1;
-		};
+		}
 	}
 
 	if (!path_to_send) {
@@ -167,7 +262,7 @@ static int execude_mode_cmd(int argc, char **argv)
 				printf("unknown option: %s\n", argv[optind]);
 				help(argv[0]);
 				return 1;
-		};
+		}
 	}
 
 	if (!mode) {
@@ -190,18 +285,26 @@ static int execude_mode_cmd(int argc, char **argv)
 	return send_mode(socket_path, m, path_to_send);
 }
 
-
 int main(int argc, char **argv)
 {
+	int err;
+
 	if (argc < 2) {
 		help(argv[0]);
 		return 1;
 	}
 	if (!strcmp(argv[1], "mode"))
-		return execude_mode_cmd(argc, argv);
+		err = execude_mode_cmd(argc, argv);
 	else if (!strcmp(argv[1], "path"))
-		return execude_path_cmd(argc, argv);
-	printf("Unknown command: %s\n", argv[1]);
-	help(argv[0]);
-	return 1;
+		err = execude_path_cmd(argc, argv);
+	else if (!strcmp(argv[1], "mount"))
+		err = execude_mount_cmd(argc, argv);
+	else {
+		printf("Unknown command: %s\n", argv[1]);
+		help(argv[0]);
+		return 1;
+	}
+	if (err)
+		printf("failed with error %d\n", err);
+	return err;
 }
