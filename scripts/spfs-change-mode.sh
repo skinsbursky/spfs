62f6c5adcde9146727b1d2186c4fec4abd77b019
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-03-30 18:58:01 +0300

    scripts: tossing some code in spfs-change-mode.sh
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- scripts/spfs-change-mode.sh
+++ scripts/spfs-change-mode.sh
@@ -2,14 +2,16 @@
 
 [[ "post-restore" == "$CRTOOLS_SCRIPT_ACTION" ]] || exit 0
 
-[ -n "$FUSE_DIR_PATTERN" ] || exit 0
-[ -n "$FUSE_CLIENT" ] || exit 1
-[ -n "$FUSE_MODE" ] || exit 2
+SPFS_CLIENT="/usr/sbin/spfs-client"
+[ -n "$SPFS_DIR_PATTERN" ] || exit 0
+[ -x "$SPFS_CLIENT" ] || exit 1
+[ -n "$SPFS_MODE" ] || exit 2
 
 set -o pipefail
 
-for sock in $FUSE_DIR_PATTERN*/*.sock; do
-	$FUSE_CLIENT mode --mode $FUSE_MODE --socket_path $sock
+for sock in $SPFS_DIR_PATTERN*/*.sock; do
+	echo "$SPFS_CLIENT mode --mode $SPFS_MODE --socket_path $sock"
+	$SPFS_CLIENT mode --mode $SPFS_MODE --socket_path $sock
 	[ $? -eq 0 ] || exit 3
 done
 
