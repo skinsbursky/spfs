e8a78508d8392b4934b46198996b905bdf0f3bb7
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-04-01 19:42:57 +0300

    manager: remove redundant send_packet_to_spfs helper
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/interface.c
+++ manager/interface.c
@@ -10,23 +10,6 @@
 #include "context.h"
 #include "interface.h"
 
-static int send_packet_to_spfs(struct spfs_manager_context_s *ctx,
-				void *package, size_t psize)
-{
-	char *socket_path;
-	int err;
-
-	socket_path = xsprintf("%s/spfs.sock", ctx->work_dir);
-	if (!socket_path)
-		return -ENOMEM;
-
-	err = send_packet(socket_path, package, psize);
-
-	free(socket_path);
-	return err;
-
-}
-
 int spfs_manager_packet_handler(void *data, void *package, size_t psize)
 {
 	struct spfs_manager_context_s *ctx = data;
@@ -37,7 +20,7 @@ int spfs_manager_packet_handler(void *data, void *package, size_t psize)
 	switch (order->cmd) {
 		case SPFS_CMD_SET_MODE:
 		case SPFS_CMD_INSTALL_PATH:
-			return send_packet_to_spfs(ctx, package, psize);
+			return send_packet(ctx->spfs_socket, package, psize);
 		case SPFS_MANAGER_MOUNT_FS:
 			return mount_fs(ctx, order->ctx, psize);
 		default:
