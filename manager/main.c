21c6aea176584cf0aac329da2873112aa4881e80
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-04-07 15:09:41 +0300

    manager: move itself to freezer root cgroup
    In some cases, manager can inherit some cgroups from the parent process.
    In case of OpenVZ it inherits containers freezer cgroup, and thus can't
    freeze/unfreeze container to replace mount points once remounted ("mount"
    moccand case).
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/main.c
+++ manager/main.c
@@ -82,6 +82,50 @@ free_spfs_socket:
 	goto free_proxy_dir;
 }
 
+static int move_to_freezer_root(void)
+{
+	char *freezer_tasks_path = "/sys/fs/cgroup/freezer/tasks";
+	char *process_id;
+	int fd, err;
+	ssize_t bytes;
+
+	fd = open(freezer_tasks_path, O_WRONLY);
+	if (fd == -1) {
+		pr_perror("failed to open %s", freezer_tasks_path);
+		return -errno;
+	}
+
+	err = -ENOMEM;
+	process_id = xsprintf("%d", getpid());
+	if (!process_id) {
+		pr_err("failed to construct string\n");
+		goto close_fd;
+	}
+
+	bytes = write(fd, process_id, strlen(process_id) + 1);
+	if (bytes < 0) {
+		pr_perror("failed to write to %s", freezer_tasks_path);
+		err = -errno;
+		goto free_process_id;
+	}
+	if (bytes != strlen(process_id) + 1) {
+		pr_err("wrote less than expected: %ld\n", bytes);
+		err = -EINTR;
+		goto free_process_id;
+
+	}
+
+	pr_info("joined freezer root cgroup\n");
+
+	err = 0;
+
+free_process_id:
+	free(process_id);
+close_fd:
+	close(fd);
+	return err;
+}
+
 int main(int argc, char *argv[])
 {
 	struct spfs_manager_context_s *ctx;
@@ -89,6 +133,11 @@ int main(int argc, char *argv[])
 	ctx = create_context(argc, argv);
 	if (!ctx)
 		return -1;
+
+	if (move_to_freezer_root()) {
+		pr_err("failed to move self to root freezer cgroup\n");
+		return -1;
+	}
 
 	if (mount_spfs(ctx))
 		return -EINVAL;
@@ -99,6 +148,7 @@ int main(int argc, char *argv[])
 			return -errno;
 		}
 	}
+
 
 	return reliable_socket_loop(ctx->sock, ctx, true, spfs_manager_packet_handler);
 }
