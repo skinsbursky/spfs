f8e9b59c564f10977635b6d1d3f649f3a84f1b96
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-04-18 20:29:09 +0300

    scripts: simplify spfs-change-mode.sh
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- scripts/spfs-change-mode.sh
+++ scripts/spfs-change-mode.sh
@@ -3,16 +3,11 @@
 [[ "post-restore" == "$CRTOOLS_SCRIPT_ACTION" ]] || exit 0
 
 SPFS_CLIENT="/usr/sbin/spfs-client"
-[ -n "$SPFS_DIR_PATTERN" ] || exit 0
 [ -x "$SPFS_CLIENT" ] || exit 1
-[ -n "$SPFS_MODE" ] || exit 2
+[ -S $SPFS_MANAGER_SOCK ] || exit 2
 
 set -o pipefail
 
-for sock in `ls $SPFS_DIR_PATTERN/spfs-manager.sock 2> /dev/null`; do
-	echo "$SPFS_CLIENT mode --mode $SPFS_MODE --socket_path $sock"
-	$SPFS_CLIENT mode --mode $SPFS_MODE --socket_path $sock
-	[ $? -eq 0 ] || exit 3
-done
-
-exit 0
+echo "$SPFS_CLIENT manage 'mode;all;mode=stub' --socket-path $SPFS_MANAGER_SOCK"
+$SPFS_CLIENT manage 'mode;all;mode=stub' --socket-path $SPFS_MANAGER_SOCK
+exit $?
