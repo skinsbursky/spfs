71519de8c523f6b9d5f0bb1bf35e4444df215ae7
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-05-04 17:12:15 +0300

    manager: restore actual fifo content
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/swap.c
+++ manager/swap.c
@@ -12,6 +12,128 @@
 #include "spfs.h"
 #include "swapfd.h"
 
+struct fifo_data {
+	struct list_head list;
+	char path[PATH_MAX];
+	int read_fd;
+	int write_fd;
+	bool loaded;
+};
+
+static struct fifo_data *create_fifo_data(const char *path)
+{
+	struct fifo_data *fifo;
+
+	fifo = malloc(sizeof(*fifo));
+	if (!fifo) {
+		pr_err("failed to allocate\n");
+		return NULL;
+	}
+
+	fifo->read_fd = open(path, O_RDONLY | O_NONBLOCK);
+	if (fifo->read_fd == -1) {
+		pr_perror("failed to open %s in read mode", path);
+		goto free_fifo;
+	}
+
+	fifo->write_fd = open(path, O_WRONLY | O_NONBLOCK);
+	if (fifo->write_fd == -1) {
+		pr_perror("failed to open %s in write mode", path);
+		goto close_read_fd;
+	}
+
+	fifo->loaded = false;
+	pr_debug("created new fifo object for %s\n", path);
+	return fifo;
+
+close_read_fd:
+	close(fifo->read_fd);
+free_fifo:
+	free(fifo);
+	return NULL;
+}
+
+static int do_fifo_magic(struct spfs_info_s *info,
+		     const char *path, const char *fd_path)
+{
+	static LIST_HEAD(fifos);
+	struct fifo_data *fifo = NULL, *tmp;
+
+	list_for_each_entry(tmp, &fifos, list) {
+		if (!strcmp(tmp->path, path)) {
+			fifo = tmp;
+			break;
+		}
+	}
+
+	if (!fifo) {
+		fifo = create_fifo_data(path);
+		if (!fifo) {
+			pr_err("failed to create fifo object\n");
+			return -EPERM;
+		}
+	}
+
+	if (!fifo->loaded) {
+		int fd;
+		ssize_t bytes;
+
+		fd = open(fd_path, O_RDONLY | O_NONBLOCK);
+		if (fd == -1) {
+			pr_perror("failed to open %s", fd_path);
+			return -errno;
+		}
+
+		bytes = fcntl(fd, F_GETPIPE_SZ);
+		if (bytes < 0) {
+			pr_perror("failed to discover %s capacity", fd_path);
+			close(fd);
+			return -1;
+		}
+
+		pr_debug("%s capacity: %ld\n", path, bytes);
+
+		bytes = tee(fd, fifo->write_fd, bytes, SPLICE_F_NONBLOCK);
+		if (bytes < 0) {
+			pr_perror("failed to splice data from %s to %s", fd_path, path);
+			close(fd);
+			return -1;
+		}
+		close(fd);
+		pr_debug("Copied %ld bytes to fifo object %s\n", bytes, path);
+	}
+	return 0;
+}
+
+static int do_file_magic(struct spfs_info_s *info,
+			 const char *path, const char *fd_path)
+{
+	char full_path[PATH_MAX];
+	struct stat st;
+
+	snprintf(full_path, PATH_MAX, "%s/%s", info->mountpoint, path);
+	if (stat(full_path, &st)) {
+		pr_perror("failed to stat %s");
+		return -errno;
+	}
+
+	switch (st.st_mode & S_IFMT) {
+		case S_IFIFO:
+			return do_fifo_magic(info, full_path, fd_path);
+		case S_IFSOCK:
+		case S_IFLNK:
+		case S_IFREG:
+		case S_IFBLK:
+		case S_IFDIR:
+		case S_IFCHR:
+			break;
+		default:
+			pr_err("unknows file mode: 0%o\n", st.st_mode & S_IFMT);
+			return -EINVAL;
+	}
+	return 0;
+}
+
 static bool test_fd(pid_t pid, int fd, char *path, void *data)
 {
 	struct spfs_info_s *info = data;
@@ -31,6 +153,9 @@ static bool test_fd(pid_t pid, int fd, char *path, void *data)
 		return false;
 	}
 	if (st.st_dev != info->spfs_stat.st_dev)
+		return false;
+
+	if (do_file_magic(info, path, fd_path))
 		return false;
 
 	pr_debug("replacing %s (-> %s)\n", fd_path, path);
