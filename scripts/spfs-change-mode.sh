e4589b10f9a9d23d81d20c2e88bf2ef40a9d24ed
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-05-18 13:48:36 +0300

    scripts: update spfs-change-mode.sh
    Make it more verbose in case of errors.

--- scripts/spfs-change-mode.sh
+++ scripts/spfs-change-mode.sh
@@ -4,13 +4,29 @@
 
 set -o pipefail
 
-[ -n $SPFS_MANAGER_SOCK ] || exit 0
-[ -n $SPFS_MODE ] || exit 0
+if [ -z "$SPFS_MANAGER_SOCK" ]; then
+	echo "SPFS_MANAGER_SOCK environment variable is not set"
+	exit 1
+fi
 
-[ -S $SPFS_MANAGER_SOCK ] || exit 1
+if [ -z "$SPFS_MODE" ]; then
+	echo "SPFS_MODE environment variable is not set"
+	exit 1
+fi
+
+[ -e $SPFS_MANAGER_SOCK ] || exit 0
+
+if [ ! -S "$SPFS_MANAGER_SOCK" ]; then
+	echo "$SPFS_MANAGER_SOCK is not a socket"
+	exit 1
+fi
 
 SPFS_CLIENT="/usr/sbin/spfs-client"
-[ -x "$SPFS_CLIENT" ] || exit 2
+
+if [ ! -x "$SPFS_CLIENT" ]; then
+	echo "Filed to find executable /usr/sbin/spfs-client"
+	exit 1
+fi
 
 $SPFS_CLIENT manage "mode;all;mode=$SPFS_MODE" --socket-path $SPFS_MANAGER_SOCK > /dev/null
 exit $?
