1bc94ae444cbe3df22610eae909d3993be804211
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-06-27 20:22:13 +0300

    replace: remove unused ns_pid from __replace_resources declaration
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/replace.h
+++ manager/replace.h
@@ -7,8 +7,7 @@ struct freeze_cgroup_s;
 
 int __replace_resources(struct freeze_cgroup_s *fg, int *ns_fds,
 			const char *source_mnt, dev_t src_dev,
-			const char *target_mnt,
-			pid_t ns_pid);
+			const char *target_mnt);
 
 int replace_resources(struct freeze_cgroup_s *fg,
 		      const char *source_mnt, dev_t src_dev,
