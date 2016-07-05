21f0fd6fda5ceee2a1ec87492810c488a1fc6e9e
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-07-05 12:02:53 +0300

    processes: rename create_file_obj into create_fd_obj
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/file_obj.h
+++ manager/file_obj.h
@@ -1,7 +1,7 @@
 #ifndef __SPFS_MANAGER_FILE_OBJ_H_
 #define __SPFS_MANAGER_FILE_OBJ_H_
 
-int create_file_obj(const char *path, unsigned flags, mode_t mode,
+int create_fd_obj(const char *path, unsigned flags, mode_t mode,
 		    const char *parent, void **file_obj);
 int get_file_obj_fd(void *file_obj, unsigned flags);
 
