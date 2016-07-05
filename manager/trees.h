8a6bc4e23a43ee15fb06c0fb15d74d4705055572
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-07-05 13:52:38 +0300

    processes: collect only unique fs
    Finction collect_process_env was renamed into collect_process_fs.
    Function collect_process_fs was renamed into collect_process_cwd_root.
    Directory openning was moved to collect_process_cwd_root.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/trees.h
+++ manager/trees.h
@@ -11,6 +11,7 @@ struct replace_fd {
 int collect_fd(pid_t pid, int fd, void *file_obj, struct replace_fd **rfd);
 pid_t fd_table_exists(pid_t pid);
 int collect_fd_table(pid_t pid);
+pid_t fs_struct_exists(pid_t pid);
 int collect_fs_struct(pid_t pid);
 int collect_map_fd(int fd, const char *path, mode_t mode, int *map_fd);
 int collect_fifo(const char *path);
