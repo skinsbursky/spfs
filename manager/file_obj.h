7cbef925f3950b5a98aecf58c09f9eb2e0c7e54a
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-07-22 18:28:20 +0300

    manager: cleanup create_map_obj usage
    Precursor patch
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/file_obj.h
+++ manager/file_obj.h
@@ -2,7 +2,7 @@
 #define __SPFS_MANAGER_FILE_OBJ_H_
 
 int create_fd_obj(const char *path, unsigned flags, mode_t mode,
-		  int source_fd, void *file_obj);
+		  int source_fd, void **file_obj);
 int get_file_obj_fd(void *file_obj, unsigned flags);
 void destroy_fd_obj(void *file_obj);
 
@@ -10,10 +10,10 @@ struct link_remap_s;
 struct replace_info_s;
 int create_file_object(const struct replace_info_s *ri,
 		const char *path, unsigned flags, mode_t mode,
-		int source_fd, void *file_obj,
+		int source_fd, void **file_obj,
 		struct link_remap_s **link_remap,
 		int (*create_obj)(const char *path, unsigned flags,
 			mode_t mode, int source_fd,
-			void *file_obj));
+			void **file_obj));
 
 #endif
