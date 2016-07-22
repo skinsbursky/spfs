9b72178d70d9df6163518014ab22ea3e0ddf6afa
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-07-22 18:28:20 +0300

    manager: remove obsolete file object helpers
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- manager/file_obj.h
+++ manager/file_obj.h
@@ -1,20 +1,7 @@
 #ifndef __SPFS_MANAGER_FILE_OBJ_H_
 #define __SPFS_MANAGER_FILE_OBJ_H_
 
-int create_fd_obj(const char *path, unsigned flags, mode_t mode,
-		  int source_fd, void **file_obj);
-int get_file_obj_fd(void *file_obj, unsigned flags);
-void destroy_fd_obj(void *file_obj);
-
-struct link_remap_s;
 struct replace_info_s;
-int create_file_object(const struct replace_info_s *ri,
-		const char *path, unsigned flags, mode_t mode,
-		int source_fd, void **file_obj,
-		struct link_remap_s **link_remap,
-		int (*create_obj)(const char *path, unsigned flags,
-			mode_t mode, int source_fd,
-			void **file_obj));
 int get_file_obj(const char *path, unsigned flags, mode_t mode, int source_fd,
 		 const struct replace_info_s *ri,
 		 void *cb_data, int (*cb)(void *cb_data, void *new_fobj, void **res_fobj),
