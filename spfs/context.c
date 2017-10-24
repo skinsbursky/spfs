06dcbba674b7a41dd8517efc69a6e67e2a51a575
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-10-24 14:04:30 +0300

    manager: improve work mode change prints
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- spfs/context.c
+++ spfs/context.c
@@ -377,18 +377,22 @@ int change_work_mode(struct spfs_context_s *ctx, spfs_mode_t mode,
 	int err;
 	spfs_mode_t cur_mode = ctx->wm->mode;
 
-	pr_info("%s: changing work mode from %d to %d (path: %s, ns_pid: %d)\n",
-			__func__, ctx->wm->mode, mode, path, ns_pid);
+	pr_info("%s: changing work mode from \"%s\" to \"%s\" "
+		"(path: %s, ns_pid: %d)\n",
+			__func__, work_modes[cur_mode], work_modes[mode],
+			path, ns_pid);
 
 	if (!stale_work_mode(mode, path)) {
-		pr_info("%s: the mode is already %d\n", __func__, ctx->wm->mode);
+		pr_info("%s: the mode is already \"%s\"\n", __func__,
+				work_modes[cur_mode]);
 		return 0;
 	}
 
 	err = set_work_mode(ctx, mode, path, ns_pid);
 	if (err)
-		pr_err("%s: changing work mode to %d (path: %s, ns_pid: %d) failed\n",
-				__func__, cur_mode, mode, path, ns_pid);
+		pr_err("%s: changing work mode to \"%s\" "
+			"(path: %s, ns_pid: %d) failed\n",
+			__func__, work_modes[mode], path, ns_pid);
 	return err;
 }
 
